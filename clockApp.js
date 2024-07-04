3760e4b0fe55328f7554a48bbe89ddbd414a1fb6
NightMare <nightmare@localhost.localdomain>
2024-07-04 14:59:20 +0600

    change some feature

--- clockApp.js
+++ clockApp.js
@@ -1,10 +1,10 @@
 const hour = document.getElementById("hour");
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const progress = document.getElementById('progress');
-document.getElementById('ampm').textContent = ampm;
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const progress = document.getElementById("progress");
+document.getElementById("ampm").textContent = ampm;
 
-function showCurrentTime(){
+function showCurrentTime() {
     let date = new Date();
     let hr = date.getHours();
     let min = date.getMinutes();
@@ -12,16 +12,13 @@ function showCurrentTime(){
     const ampm = hr >= 12 ? "PM" : "AM";
 
     hr = hr % 12;
-    hr = hr ? hr : 12; 
+    hr = hr ? hr : 12;
 
     hour.textContent = hr;
     minute.textContent = min;
     second.textContent = sec;
     document.getElementById("ampm").textContent = ampm;
 
-    progress.style.width = (sec/60) * 100 + '%'
-
-    ampm.style.color = white
-
+    progress.style.width = (sec / 60) * 100 + "%";
 }
-setInterval(showCurrentTime, 1000)
+setInterval(showCurrentTime, 1000);
