800787f2aee03f0619e901f074e7b38fe3fd4cf1
NightMare <nightmare@localhost.localdomain>
2024-07-04 14:17:06 +0600

    Clock Project Done

---
+++ clockApp.js
@@ -0,0 +1,27 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const progress = document.getElementById('progress');
+document.getElementById('ampm').textContent = ampm;
+
+function showCurrentTime(){
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    const ampm = hr >= 12 ? "PM" : "AM";
+
+    hr = hr % 12;
+    hr = hr ? hr : 12; 
+
+    hour.textContent = hr;
+    minute.textContent = min;
+    second.textContent = sec;
+    document.getElementById("ampm").textContent = ampm;
+
+    progress.style.width = (sec/60) * 100 + '%'
+
+    ampm.style.color = white
+
+}
+setInterval(showCurrentTime, 1000)
